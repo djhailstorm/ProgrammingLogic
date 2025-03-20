b478b467905013db6d19c836daa9d2f14a221aa6
Brian Kummerer <djhailstorm@yahoo.com>
2025-03-20 09:45:00 -0400

    Lab1 Completed

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,7 @@ class Program
   static void Main(string[] args)
   {
     // Step 1
-    Console.WriteLine(Brian Kummerer);
+    Console.WriteLine("Brian Kummerer");
     // Step 2
     /* Name:  Brian Kummerer
        Title: IT-1050 - Lab1  
@@ -16,17 +16,20 @@ class Program
     double previousPrograms = 99.5;
     bool programmingExp = true;
     // Step 4
-    Console.WriteLine("My favorite number" is + favoriteNumber);
-    Console.WriteLine("My favorite Programming Language is" + favoriteLanguage);
-    Console.WriteLine("I have prefiously written over" + previousPrograms + "Programs");
+    Console.WriteLine("My favorite number is " + favoriteNumber);
+    Console.WriteLine("My favorite Programming Language is " + favoriteLanguage);
+    Console.WriteLine("I have previously written over " + previousPrograms + " Programs");
     if (programmingExp = true)
         {
         Console.WriteLine("I do have previous Programming Experience");
         } 
     else if (programmingExp = false) 
         {
-        Console.WirteLine("I do not have previous Programming Experience");    
+        Console.WriteLine("I do not have previous Programming Experience");    
         } 
+    //Step 5
+    const string schoolName = "Cuyahoga Community College";
+    Console.Write(schoolName);
 
   }
 
