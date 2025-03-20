933ef6a3c9b6225f90e25acb05b7352cc9522be7
Brian Kummerer <djhailstorm@yahoo.com>
2025-03-20 09:14:17 -0400

    Lab1 step3

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,7 +1,33 @@
 using System;
 class Program
 {
- static void Main() {
-    Console.WriteLine("Hello, world!");
- }
+  static void Main(string[] args)
+  {
+    // Step 1
+    Console.WriteLine(Brian Kummerer);
+    // Step 2
+    /* Name:  Brian Kummerer
+       Title: IT-1050 - Lab1  
+    */
+    //   
+    // Step 3 
+    int favoriteNumber = 7;
+    string favoriteLanguage = "C++";
+    double previousPrograms = 99.5;
+    bool programmingExp = true;
+    // Step 4
+    Console.WriteLine("My favorite number" is + favoriteNumber);
+    Console.WriteLine("My favorite Programming Language is" + favoriteLanguage);
+    Console.WriteLine("I have prefiously written over" + previousPrograms + "Programs");
+    if (programmingExp = true)
+        {
+        Console.WriteLine("I do have previous Programming Experience");
+        } 
+    else if (programmingExp = false) 
+        {
+        Console.WirteLine("I do not have previous Programming Experience");    
+        } 
+
+  }
+
 }
