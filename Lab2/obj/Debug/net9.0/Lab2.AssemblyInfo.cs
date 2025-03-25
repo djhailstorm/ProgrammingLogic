d675668d1a14edb6fea944a038509f1cd3b78450
Brian Kummerer <djhailstorm@yahoo.com>
2025-03-25 00:10:52 -0400

    space

--- Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944e4a48051295b1eb4de993a59e89805559c29e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12cfa1096bc43f9bc2ac732220e8890ae35a91c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
